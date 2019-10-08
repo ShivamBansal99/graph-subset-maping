ca9c2783a10e2c6b7c1a3c54ca926feeeb3499e0
Shivam Bansal <cs1170376@iitd.ac.in>
2019-10-08 18:31:37 +0530

    initial commit

---
+++ first.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define vvi vector<vi>
+
+using namespace std ;
+
+void printMat(vvi x){
+        for(auto i:x){
+                for(auto j:i)
+                        cout<<j<<" ";
+                cout<<endl ;
+        }
+}
+
+vector<int> get_all_edges(vector<vector<int> > g, int n,int i){
+    vector<int> edges;
+    for(int j=0;j<n;j++){
+        if(g[i][j]>0) edges.push_back(j);
+    }
+    return edges;
+}
+
+int main(int argc,char *argv[]){
+        string test=argv[1] ;
+        string input=test+".graphs" ;
+        string output=test+".satinput" ;
+        freopen(input.c_str(), "r", stdin);
+        freopen(output.c_str(), "w", stdout);
+        int a,b,temp,M=-1,N=-1 ;
+        vvi gEdges,gDashEdges ;
+        bool first=true,change=false ;
+        while(cin>>temp){
+                if(!first){ //scanned b
+                        b=temp ;
+                        if(a==0&&b==0){
+                                change=true ;
+                                first=!first ;
+                                continue;
+                        }
+                        if(!change){        //for edges of G
+                                if(max(a,b)>N)
+                                        N=max(a,b) ;                        //N
+                                gEdges.push_back(vi(2,0)) ;
+                                gEdges[gEdges.size()-1][0]=a ;
+                                gEdges[gEdges.size()-1][1]=b ;
+                        }
+                        else{                        //for Edges of GDash
+                                if(max(a,b)>M)
+                                        M=max(a,b) ;                        //M
+                                gDashEdges.push_back(vi(2,0)) ;
+                                gDashEdges[gDashEdges.size()-1][0]=a ;
+                                gDashEdges[gDashEdges.size()-1][1]=b ;                                
+                        }
+                }
+                a=temp ; //scanned a
+                first=!first ;
+        }
+        ofstream NM ;
+        NM.open(test+".NM") ;
+        NM<<N<<" "<<M<<endl ;
+        NM.close() ;
+        vvi g(N,vi(N,0)) ;
+        vvi g_dash(M,vi(M,0)) ;
+        for(auto i:gEdges)
+                g[i[0]-1][i[1]-1]=1 ;
+        for(auto i:gDashEdges)
+                g_dash[i[0]-1][i[1]-1]=1 ;
+        printMat(g) ;
+        printMat(g_dash) ;
+        
+        for(int i=0;i<M;i++){
+                for(int j=0;j<N;j++){
+                        cout<<i*N+j+1<<' ';
+                }
+                cout<<'0'<<endl;
+                for(int j=0;j<N;j++){
+                        for(int k=j+1;k<N;k++){
+                                cout<<-1*(i*N+j+1)<<' '<<-1*(i*N+k+1)<<' '<<'0'<<'\n';
+                        }
+                }
+        }
+        for(int i=0;i<N;i++){
+                for(int j=0;j<M;j++){
+                        for(int k=j+1;k<M;k++){
+                                cout<<-1*(j*N+i+1)<<' '<<-1*(k*N+i+1)<<' '<<'0'<<'\n';
+                        }
+                }
+        }
+        for(int i=0;i<M;i++){
+                for(int j=0;j<N;j++){
+                        vector<int> y = get_all_edges(g,N,j);
+                        vector<int> z = get_all_edges(g_dash,M,i);
+                        for(int k=0;k<z.size();k++){
+                                cout<<-1*(i*N+j+1)<<' ';
+                                for(int r=0;r<y.size();r++){
+                                        cout<<(z[k]*N+y[r]+1)<<' ';
+                                }
+                                cout<<' '<<'0'<<endl;
+                        }
+                }
+        }
+}
---
+++ result.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc,char* argv[]) {
+        int N,M ;
+        if(argc<2){
+                cerr<<"Unsufficient number of arguments" ;
+                return 0 ;
+        }
+        string test=argv[1] ;
+        ifstream NM ;
+        NM.open(test+".NM") ;
+        NM>>N>>M ;
+        NM.close() ;
+        string input = test+".satoutput" ;
+        string output = test+".mapping" ;
+        freopen(input.c_str(), "r", stdin);
+        freopen(output.c_str(), "w", stdout);        
+        string s;
+        cin>>s;
+        vector<vector<int> > maping;
+        for(int i=0;i<M;i++){
+                int flag =0;
+                vector<int> t;
+                maping.push_back(t);
+                for(int j=0;j<N;j++){
+                        int k;
+                        cin>>k;
+                        if(k>0) k=1;
+                        else k=0;
+                        maping[i].push_back(k);
+                        
+                        if(maping[i][j]>0 && flag>0){
+                                cout<<'0';
+                                return 0;
+                        }
+                        if(maping[i][j]>0){
+                                flag=1;
+                        }
+                        
+                }
+        }
+        for(int i=0;i<M;i++){
+                int index =0;
+                for(int j=0;j<N;j++){
+                        if(maping[i][j]>0){
+                                cout<<i+1<<' '<<j+1<<endl;
+                                break;
+                        }
+                }
+        }
+        return 0;
+}
